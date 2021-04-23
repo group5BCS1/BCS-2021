6a97c51b66628a4cddc2abcbdd217cf8fdb53007
group5BCS1 <okwakundagglosh@gmail.com>
2021-04-23 15:06:20 +0300

    chapter 5 exercise 3, 2nd commit

--- src/charpter 5/exercise 3.py
+++ src/charpter 5/exercise 3.py
@@ -10,5 +10,16 @@ dimes = 25
 quarters = 25
 ones = 0
 fives = 0
-
 price_str = input("\nEnter the purchase price (xx.xx) or 'q' to quit: ")
+while price_str != 'q':
+    price_flo = float(price_str)
+    total_cents = round(price_flo * 100)
+    dollars = total_cents // 100
+    cents = total_cents - (dollars * 100)
+
+    if total_cents % 5 == 0 and total_cents > 0:
+        print("\nMenu for deposits:\n  'n' - deposit a nickel\n  'd' - deposit a dime",
+              "\n  'q' - deposit a quarter\n  'o' - deposit a one dollar bill\n  'f' - deposit a five dollar bill", \
+              "\n  'c' - cancel the purchase")
+        print("\nPayment due: ", dollars, "dollars and ", cents, "cents")
+        deposit_str = input("Indicate your deposit: ")
