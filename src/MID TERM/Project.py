210843e41ef1c62213fe143db50f0c34bf2bb83c
group5BCS1 <okwakundagglosh@gmail.com>
2021-05-03 16:22:17 +0300

    simple version of the program

---
+++ src/MID TERM/Project.py
@@ -0,0 +1,5 @@
+print("===================RESTART===================")
+print("You're welcome our esteemed customer.")
+while True:
+    a = input("Enter your code: ")
+    print("Customer's code:" , a)
